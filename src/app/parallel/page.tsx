c2bd1ad50392a6563533fbc0c44a1e2b90c061d0
Sehab Ahmed <sehab@MacBook-Pro-Sehab.local>
2025-07-08 11:01:27 +0200

    parallel route created

---
+++ src/app/parallel/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="max-w-7xl mx-auto flex flex-col gap-5">
+      <div className="bg-amber-500 h-[400px] w-full flex justify-center items-center">
+        <h1 className="text-2xl text-center text-default">Static Content</h1>
+      </div>
+    </div>
+  );
+}
