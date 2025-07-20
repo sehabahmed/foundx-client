58eb8042e75195d4a3ecbe9f683fb00701518e20
Sehab Ahmed <sehab@MacBook-Pro-Sehab.local>
2025-07-20 10:24:56 +0200

    successfully setup next auth middleware with specific user

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Link } from "@heroui/link";
+import React from "react";
 
 export default function page() {
   return (
-    <div>
-      Login Page
+    <div className="flex h-[calc(100vh-200px)] w-full flex-col items-center justify-center">
+      <h2 className="text-2xl text-center font-bold">Login with FoundX</h2>
+      <p className="mb-4">Welcome Back! Let&lsquo;s Get Started</p>
+      <div className="w-[35%]">
+        <form>
+          <div className="py-3">
+            <Input type="text" />
+          </div>
+          <div className="py-3">
+            <Input type="text" />
+          </div>
+
+          <Button
+            className="w-full my-3 font-semibold bg-default-900 text-default"
+            radius="sm"
+            size="lg"
+          >
+            Login
+          </Button>
+        </form>
+        <h3 className="text-center">
+          Don&lsquo;t have an account? <Link href="/register">Register</Link>
+        </h3>
+      </div>
     </div>
-  )
+  );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,22 +4,48 @@ import { NextResponse } from "next/server";
 
 const AuthRoutes = ["/register", "/login"];
 
+type TRole = keyof typeof roleBaseRoutes;
+
+const roleBaseRoutes = {
+  USER: [/^\/profile/],
+  ADMIN: [/^\/admin/],
+};
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  console.log(pathname);
+
 //   const user = {
 //     name: "sehab",
-//     toke: "abc def",
-//     role: "user",
+//     token: "abc def",
+//     role: "ADMIN",
 //   };
 
-const user = undefined;
+    const user = undefined;
 
-  
+  if (!user) {
+    if (AuthRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  if (user?.role && roleBaseRoutes[user?.role as TRole]) {
+    const routes = roleBaseRoutes[user?.role as TRole];
+
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+
+  }
 
   return NextResponse.redirect(new URL("/", request.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/profile", "/admin"],
+  matcher: ["/profile", "/admin", "/register", "/login"],
 };
